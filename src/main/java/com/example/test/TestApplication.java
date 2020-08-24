f646d50ce941f01aab65fd14ee79dadd1ee189ab
Lay-lili <2918590791@qq.com>
2020-08-24 14:59:43 +0800

    2.0版

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * git测试2.0
+ */
 @SpringBootApplication
 @MapperScan("com.example.test.mapper")
 public class TestApplication {
